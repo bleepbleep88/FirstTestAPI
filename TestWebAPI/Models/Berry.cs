d904d03ac78992626bcdd1289a9ee67b01d6dd9e
Ian Kelly <ikelly@uwyo.edu>
2021-06-02 23:35:11 -0800

    Added getBerryList Task

--- TestWebAPI/Models/Berry.cs
+++ TestWebAPI/Models/Berry.cs
@@ -8,5 +8,14 @@ namespace TestWebAPI.Models
     public class Berry
     {
         public String name { get; set; }
+        public string url { get; set; }
+    }
+
+    public class BerryRoot
+    {
+        public int count { get; set; }
+        public string next { get; set; }
+        public object previous { get; set; }
+        public List<Berry> results { get; set; }
     }
 }
