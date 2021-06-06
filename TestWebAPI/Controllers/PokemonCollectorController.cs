01dd64f63d856a16a2c62036e67ad217d3b6b9aa
Ian Kelly <ikelly@uwyo.edu>
2021-06-05 16:37:38 -0800

    Added Pokemon List Task

---
+++ TestWebAPI/Controllers/PokemonCollectorController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TestWebAPI.Models;
+
+namespace TestWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PokemonCollectorController : ControllerBase
+    {
+
+        //Should return a list of first 150 pokemon
+        public async Task<List<Pokemon>> getPokemonList()
+        {
+            List<Pokemon> PokemonList = new List<Pokemon>();
+            HttpClient httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("https://pokeapi.co/api/v2/pokemon");
+            HttpResponseMessage response = await httpClient.GetAsync(httpClient.BaseAddress);
+            PokemonRoot result = JsonConvert.DeserializeObject<PokemonRoot>(await response.Content.ReadAsStringAsync());
+            PokemonList.AddRange(result.results);
+            while(PokemonList.Count < 151)
+            {
+                HttpClient newClient = new HttpClient();
+                newClient.BaseAddress = new Uri(result.next);
+                response = await newClient.GetAsync(newClient.BaseAddress);
+                result = JsonConvert.DeserializeObject<PokemonRoot>(await response.Content.ReadAsStringAsync());
+                PokemonList.AddRange(result.results);
+            }
+            PokemonList.RemoveRange(151, PokemonList.Count - 151);
+            return PokemonList;
+        }
+    }
+}
---
+++ TestWebAPI/Models/Pokemon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestWebAPI.Models
+{
+    public class Pokemon
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+    }
+
+    public class PokemonRoot
+    {
+        public int count { get; set; }
+        public string next { get; set; }
+        public object previous { get; set; }
+        public List<Pokemon> results { get; set; }
+    }
+}
